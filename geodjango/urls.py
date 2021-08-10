c688a3c720b729b95eb928e2379d1959b7996d94
suhailvs <suhailvs@gmail.com>
2021-08-10 16:57:39 +0530

    added distance search

--- geodjango/urls.py
+++ geodjango/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from world import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.Home.as_view())
 ]
+
+
--- world/views.py
+++ world/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from django.views import generic
+from django.contrib.gis.geos import fromstr
+from django.contrib.gis.db.models.functions import Distance
+from .models import Shop
+from django.contrib.gis.geos import GEOSGeometry, LineString, Point
+
+longitude = 76.2332137
+latitude = 10.4977121
+
+user_location = Point(longitude, latitude, srid=4326)
+
+
+
+class Home(generic.ListView):
+    model = Shop
+    context_object_name = 'shops'
+    queryset = Shop.objects.annotate(distance=Distance('location',
+    user_location)
+    ).order_by('distance')[0:6]
+    template_name = 'index.html'
